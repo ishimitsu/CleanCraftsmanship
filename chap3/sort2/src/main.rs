e2f4ca0b9b9e299095c2c55a191a0c36c1b4f15d
Mitsutaka Shiraishi <srismttk@gmail.com>
2022-11-28 00:47:37 +0900

    make sort2

---
+++ chap3/sort2/src/main.rs
@@ -0,0 +1,28 @@
+pub fn sort(mut vec: Vec<i32>) -> Vec<i32> {
+    if vec.len() > 1 {
+        for limit in (0..vec.len()).rev() {        
+            for idx in 0..vec.len() {
+                let next_idx = idx + 1;
+                if next_idx == vec.len() { break };
+                if vec[idx] > vec[next_idx] {
+                    let tmp: i32 = vec[idx];
+                    vec[idx] = vec[next_idx];
+                    vec[next_idx] = tmp;
+                }
+            }
+        }
+    }
+
+    return vec;
+}
+
+#[cfg(test)]
+mod sort_test {
+    use super::*;
+
+    #[test]
+    fn sorted() {
+        let expect = vec![1, 2, 3];
+        assert_eq!(expect, sort(vec![2, 3, 1]));
+    }
+}
