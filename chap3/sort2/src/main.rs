058149501f8d9224256b3f0d74d687ff7d6020de
Mitsutaka Shiraishi <srismttk@gmail.com>
2022-11-28 00:57:16 +0900

    refer first, second value.

--- chap3/sort2/src/main.rs
+++ chap3/sort2/src/main.rs
@@ -1,5 +1,11 @@
 pub fn sort(mut vec: Vec<i32>) -> Vec<i32> {
-    return vec;
+    if vec.len() <= 1 { return vec; }
+
+    let fst = vec[0];
+    let sec = vec[1];
+    if fst > sec { return vec![sec, fst]; }
+
+    return vec![fst, sec];
 }
 
 #[cfg(test)]
@@ -10,6 +16,6 @@ mod sort_test {
     fn test_sort() {
         assert_eq!(vec![0], sort(vec![0]));
         assert_eq!(vec![1], sort(vec![1]));
-        assert_eq!(vec![1, 2], sort(vec![1, 2]));                
+        assert_eq!(vec![1, 2], sort(vec![2, 1]));                
     }
 }
