672ace913d9e620436046fc3b7bdb3d2714000b7
Mitsutaka Shiraishi <srismttk@gmail.com>
2022-11-28 01:04:40 +0900

    add 3 elements tests.

--- chap3/sort2/src/main.rs
+++ chap3/sort2/src/main.rs
@@ -1,11 +1,14 @@
-pub fn sort(mut vec: Vec<i32>) -> Vec<i32> {
+pub fn sort(vec: Vec<i32>) -> Vec<i32> {
     if vec.len() <= 1 { return vec; }
 
-    let fst = vec[0];
-    let sec = vec[1];
-    if fst > sec { return vec![sec, fst]; }
+    if vec.len() == 2 {
+        let fst = vec[0];
+        let sec = vec[1];
+        if fst > sec { return vec![sec, fst]; }
+        else { return vec![fst, sec]; }
+    }
 
-    return vec![fst, sec];
+    return vec;
 }
 
 #[cfg(test)]
@@ -16,6 +19,7 @@ mod sort_test {
     fn test_sort() {
         assert_eq!(vec![0], sort(vec![0]));
         assert_eq!(vec![1], sort(vec![1]));
-        assert_eq!(vec![1, 2], sort(vec![2, 1]));                
+        assert_eq!(vec![1, 2], sort(vec![2, 1]));
+        assert_eq!(vec![1, 2, 3], sort(vec![1, 2, 3]));
     }
 }
