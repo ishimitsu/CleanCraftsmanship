552fada52751d559c75d6e6e018d972607ab56ee
Mitsutaka Shiraishi <srismttk@gmail.com>
2022-11-28 01:22:19 +0900

    add 3 elements test.

--- chap3/sort2/src/main.rs
+++ chap3/sort2/src/main.rs
@@ -40,5 +40,7 @@ mod sort_test {
         assert_eq!(vec![1], sort(vec![1]));
         assert_eq!(vec![1, 2], sort(vec![2, 1]));
         assert_eq!(vec![1, 2, 3], sort(vec![2, 1, 3]));
+        assert_eq!(vec![1, 2, 3], sort(vec![1, 3, 2]));
+        assert_eq!(vec![1, 2, 3], sort(vec![3, 2, 1]));
     }
 }
