916f1c4fb0f96ae07dca518967b9e16560273bc2
Mitsutaka Shiraishi <srismttk@gmail.com>
2022-12-07 01:47:14 +0900

    add  sec and third day test, but this mutable is not good.

--- chap4/video_store/src/main.rs
+++ chap4/video_store/src/main.rs
@@ -2,14 +2,14 @@ pub struct Customer {
 }
 
 impl Customer {
-    fn add_rental(&self, title: &str, days: i32) {
+    fn add_rental(&mut self, title: &str, days: i32) {
     }
 
-    fn get_rental_fee(&self) -> f64 {
+    fn get_rental_fee(&mut self) -> f64 {
         1.5
     }
 
-    fn get_rental_point(&self) -> i32 {
+    fn get_rental_point(&mut self) -> i32 {
         1
     }
 }
@@ -19,16 +19,26 @@ impl Customer {
 mod customer_test {
     use super::*;
 
-    fn assertFeeAndPoints(c: Customer, fee: f64, point: i32) {
+    fn assertFeeAndPoints(c: &mut Customer, fee: f64, point: i32) {
         assert_eq!(1.5, c.get_rental_fee());
         assert_eq!(1, c.get_rental_point());
     }
 
     #[test]
     pub fn regularmovie_oneday() {
-        let c = Customer {};
+        let mut c = Customer {};
 
         c.add_rental("Regular Movie", 1);
-        assertFeeAndPoints(c, 1.5, 1);
+        assertFeeAndPoints(&mut c, 1.5, 1);
+    }
+
+    #[test]
+    pub fn regularmovie_sec_and_third_day_free() {
+        let mut c = Customer {};
+
+        c.add_rental("Regular Movie", 2);
+        assertFeeAndPoints(&mut c, 1.5, 1);
+        c.add_rental("Regular Movie", 3);
+        assertFeeAndPoints(&mut c, 1.5, 1);
     }
 }
