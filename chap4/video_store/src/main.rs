abae620d75b93eae4abba94ea18a58fb6206dab0
Mitsutaka Shiraishi <srismttk@gmail.com>
2022-12-11 23:12:03 +0900

    add Rental, due to adjust multi-video rental.

--- chap4/video_store/src/main.rs
+++ chap4/video_store/src/main.rs
@@ -33,40 +33,62 @@ impl VideoRegistory {
     }
 }
 
-pub struct Customer {
+pub struct Rental {
     title: String,
     days: i32,
+    video_type: VideoType,
+}
+
+impl Rental {
+
+}
+
+pub struct Customer {
+    rentals: Vec<Rental>,
 }
 
 impl Customer {
-    //fn default() -> Self { Self {days: 3, title:String::from("")} }
+    fn default() -> Self {
+        Self {rentals: Vec::new()}
+    }
 
     fn add_rental(&mut self, title: &str, days: i32) {
-        self.days = days;
-        self.title = title.to_string();
+        let mut video_registory = VideoRegistory::default();
+        self.rentals.push(Rental {title: title.to_string(), days: days,
+                                  video_type: video_registory.get_type(&title.to_string())});
     }
 
     fn get_rental_fee(&mut self) -> i32 {
+        let mut fee: i32 = 0;
         let mut video_registory = VideoRegistory::default();
-        match video_registory.get_type(&self.title)
-        {
-            REGULAR => self.apply_grace_period(150, 3),
-            CHILDRENS => 100,
+
+        for rental in &self.rentals {
+            match video_registory.get_type(&rental.title)
+            {
+                REGULAR => fee = fee + Customer::apply_grace_period(150, rental.days, 3),
+                CHILDRENS => fee = fee + rental.days * 100,
+            }
         }
+        fee
     }
 
     fn get_rental_point(&mut self) -> i32 {
+        let mut point: i32 = 0;
         let mut video_registory = VideoRegistory::default();
-        match video_registory.get_type(&self.title)
-        {
-            REGULAR => self.apply_grace_period(1, 3),
-            CHILDRENS => 1,
+
+        for rental in &self.rentals {
+            match video_registory.get_type(&rental.title)
+            {
+                REGULAR => point = point + Customer::apply_grace_period(1, rental.days, 3),
+                CHILDRENS => point = point + 1,
+            }
         }
+        point
     }
 
-    fn apply_grace_period(&mut self, amount: i32, grace: i32) -> i32 {
-        if self.days > grace {
-            return amount + amount * (self.days - grace);
+    fn apply_grace_period(amount: i32, days: i32, grace: i32) -> i32 {
+        if days > grace {
+            return amount + amount * (days - grace);
         }
         amount
     }
@@ -83,7 +105,7 @@ mod customer_test {
 
     #[test]
     pub fn regularmovie_oneday() {
-        let mut c = Customer{days: 3, title:String::from("")};
+        let mut c = Customer::default();
 
         c.add_rental("RegularMovie", 1);
         assert_fee_and_points(&mut c, 150, 1);
@@ -91,7 +113,7 @@ mod customer_test {
 
     #[test]
     pub fn regularmovie_sec_and_third_day_free() {
-        let mut c = Customer{days: 3, title:String::from("")};
+        let mut c = Customer::default();
 
         c.add_rental("RegularMovie", 2);
         assert_fee_and_points(&mut c, 150, 1);
@@ -101,7 +123,7 @@ mod customer_test {
 
     #[test]
     pub fn regularmovie_four_days() {
-        let mut c = Customer{days: 3, title:String::from("")};
+        let mut c = Customer::default();
 
         c.add_rental("RegularMovie", 4);
         assert_fee_and_points(&mut c, 300, 2);
@@ -109,7 +131,7 @@ mod customer_test {
 
     #[test]
     pub fn childrens_movie_one_days() {
-        let mut c = Customer{days: 3, title:String::from("")};
+        let mut c = Customer::default();
 
         c.add_rental("ChildrenMovie", 1);
         assert_fee_and_points(&mut c, 100, 1);
@@ -117,7 +139,7 @@ mod customer_test {
 
     #[test]
     pub fn childrens_movie_four_days() {
-        let mut c = Customer{days: 3, title:String::from("")};
+        let mut c = Customer::default();
 
         c.add_rental("ChildrenMovie", 4);
         assert_fee_and_points(&mut c, 400, 1); // 400 is for using 3 at get_rental_point
@@ -125,7 +147,7 @@ mod customer_test {
 
     #[test]
     pub fn one_regular_one_childrens_four_days() {
-        let mut c = Customer{days: 3, title:String::from("")};
+        let mut c = Customer::default();
 
         c.add_rental("RegularMovie", 4); // $3 + 2p
         c.add_rental("ChildrenMovie", 4); // $4 + 1p
